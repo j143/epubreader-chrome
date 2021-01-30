78b2252e9a41b216f2019145e0b905ff9542e1a9
vite <magisterium.vs@list.ru>
2021-01-30 03:31:50 +0300

    added webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'node_modules/epubjs-reader/reader',
+                    to: './',
+                    toType: 'dir'
+                },
+                {
+                    from: 'src', 
+                    to: './', 
+                    toType: 'dir', 
+                    force: true,
+                    globOptions: { ignore: ['**/index.js'] }
+                }
+            ],
+            options: {
+                concurrency: 100,
+            },
+        }),
+    ],
+};
