0ce67a7c5bb88642546d7f52e2a70d2fdd90cc40
vite <magisterium.vs@list.ru>
2021-02-02 19:26:16 +0300

    added storage function support

--- src/js/content.js
+++ src/js/content.js
@@ -5,23 +5,7 @@ document.onreadystatechange = function () {
     if (document.readyState !== "complete")
         return;
 
-    if (location.href.includes("?bookPath=data")) {
-        
-        window.reader = ePubReader(location.href, {
-            restore: true,
-            encoding: 'base64'
-        });
-
-    } else if (location.href.includes("?bookPath")) {
-
-        window.reader = ePubReader(location.href, {
-            restore: true
-        });
-    }
-
-    // this part of the code actually duplicates the basic implementation of 
-    // the upload controller: https://github.com/intity/epubjs-reader/blob/master/src/controllers/upload_controller.js
-
+    var reader;
     var upload = document.getElementById('upload');
 
     upload.addEventListener('change', function(e) {
@@ -29,26 +13,38 @@ document.onreadystatechange = function () {
         if (e.target.files.length === 0)
             return;
 
-        var file = e.target.files[0];
-        var fileReader = new FileReader();
-
-        fileReader.readAsArrayBuffer(file);
-        fileReader.onload = function (e) {
-            window.reader = ePubReader(e.target.result, {
-                restore: true
-            });
-        };
-
-        // the issue of loading epub from base64 format is still not closed: https://github.com/futurepress/epub.js/issues/731
-
-        // fileReader.readAsDataURL(file);
-        // fileReader.onload = function(e) {
-        //     var readerUrl = chrome.runtime.getURL("index.html") + "?bookPath=" + e.target.result;
-        //     chrome.tabs.update({ url: readerUrl });
-        // };
-
-        fileReader.onerror = function() {
-            console.log(fileReader.error);
-        };
+        var readerUrl = chrome.runtime.getURL("index.html");
+        chrome.tabs.update({ url: readerUrl });
     });
+
+    if (location.href.includes("?bookPath")) {
+
+        reader = ePubReader(location.href, {
+            restore: true
+        });
+
+        return;
+    }
+
+    var storage = new EPUBJS.storage();
+
+    storage.init(function () {
+
+        storage.get(function (data) {
+
+            if (data !== undefined) {
+
+                reader = ePubReader(data, { restore: true });
+
+            } else {
+
+                reader = ePubReader('https://s3.amazonaws.com/moby-dick/', {
+
+                    restore: true
+                });
+            }
+        });
+    });
+
+    window.reader = reader;
 };
