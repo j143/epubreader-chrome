8588d2801b4b1b299991d214bd0582dd827819a8
vite <magisterium.vs@list.ru>
2021-02-02 21:21:28 +0300

    refactoring the upload feature

--- src/js/content.js
+++ src/js/content.js
@@ -5,28 +5,50 @@ document.onreadystatechange = function () {
     if (document.readyState !== "complete")
         return;
 
-    var reader;
+    var storage = new EPUBJS.storage();
     var upload = document.getElementById('upload');
 
     upload.addEventListener('change', function(e) {
 
         if (e.target.files.length === 0)
             return;
+        
+        if (window.FileReader) {
 
-        var readerUrl = chrome.runtime.getURL("index.html");
-        chrome.tabs.update({ url: readerUrl });
+            var fr = new FileReader();
+            fr.onload = function (e) {
+
+                window.reader = ePubReader(e.target.result, { restore: true });
+
+                storage.init(function () {
+
+                    storage.clear();
+                    storage.set(e.target.result);
+                });
+            };
+            fr.readAsArrayBuffer(e.target.files[0]);
+
+            if (location.href.includes("?bookPath")) {
+            
+                var readerUrl = chrome.runtime.getURL("index.html");
+                chrome.tabs.update({ url: readerUrl });
+            }
+
+        } else {
+
+            alert("Your browser does not support the required features.\n" +
+                "Please use a modern browser such as Google Chrome, or Mozilla Firefox.");
+        }
     });
 
     if (location.href.includes("?bookPath")) {
 
-        reader = ePubReader(location.href, {
+        window.reader = ePubReader(location.href, {
             restore: true
         });
 
         return;
     }
-
-    var storage = new EPUBJS.storage();
 
     storage.init(function () {
 
@@ -34,17 +56,15 @@ document.onreadystatechange = function () {
 
             if (data !== undefined) {
 
-                reader = ePubReader(data, { restore: true });
+                window.reader = ePubReader(data, { restore: true });
 
             } else {
 
-                reader = ePubReader('https://s3.amazonaws.com/moby-dick/', {
+                window.reader = ePubReader('https://s3.amazonaws.com/moby-dick/', {
 
                     restore: true
                 });
             }
         });
     });
-
-    window.reader = reader;
 };
